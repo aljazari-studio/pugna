28ce153c580a55b18cc9b9913a40ff34e5e000e3
Fathur Hidayat <fathur.hidayat@gmail.com>
2019-02-04 10:42:16 +0700

    Add simple modal

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -387,4 +387,34 @@ function closeAllSelect(elmnt) {
 }
 
 document.addEventListener("click", closeAllSelect);
-document.addEventListener("keyup", closeAllSelect);
+document.addEventListener("keyup", closeAllSelect); // Modal
+
+var body = document.querySelector("body");
+var pugnaOverlay = document.createElement("div");
+pugnaOverlay.setAttribute("class", "pgn-overlay");
+body.appendChild(pugnaOverlay);
+var modalTrigger = document.querySelectorAll("[data-modal]");
+var modalDismiss = document.querySelectorAll(".modal-dismiss");
+
+for (var mt = 0; mt < modalTrigger.length; mt++) {
+  modalTrigger[mt].addEventListener("click", function () {
+    var modal = document.querySelector(this.dataset.modal);
+    pugnaOverlay.classList.add("show");
+    modal.classList.add("show");
+
+    for (var md = 0; md < modalDismiss.length; md++) {
+      modalDismiss[md].addEventListener("click", function () {
+        dismissModal(modal, pugnaOverlay);
+      });
+    }
+
+    pugnaOverlay.addEventListener("click", function () {
+      dismissModal(modal, pugnaOverlay);
+    });
+  });
+}
+
+function dismissModal(modal, overlay) {
+  overlay.classList.remove("show");
+  modal.classList.remove("show");
+}
--- src/js/main.js
+++ src/js/main.js
@@ -350,6 +350,7 @@ for (let tbH = 0; tbH < tabNavItem.length; tbH++) {
 
 // Select
 var selectsCustom = document.getElementsByClassName("pgn-select");
+
 for (let sc = 0; sc < selectsCustom.length; sc++) {
   var selElmnt = selectsCustom[sc].getElementsByTagName("select")[0];
 
@@ -435,3 +436,37 @@ function closeAllSelect(elmnt) {
 
 document.addEventListener("click", closeAllSelect);
 document.addEventListener("keyup", closeAllSelect);
+
+// Modal
+var body = document.querySelector("body");
+var pugnaOverlay = document.createElement("div");
+
+pugnaOverlay.setAttribute("class", "pgn-overlay");
+body.appendChild(pugnaOverlay);
+
+var modalTrigger = document.querySelectorAll("[data-modal]");
+var modalDismiss = document.querySelectorAll(".modal-dismiss");
+
+for (let mt = 0; mt < modalTrigger.length; mt++) {
+  modalTrigger[mt].addEventListener("click", function() {
+    let modal = document.querySelector(this.dataset.modal);
+
+    pugnaOverlay.classList.add("show");
+    modal.classList.add("show");
+
+    for (let md = 0; md < modalDismiss.length; md++) {
+      modalDismiss[md].addEventListener("click", function() {
+        dismissModal(modal, pugnaOverlay);
+      });
+    }
+
+    pugnaOverlay.addEventListener("click", function() {
+      dismissModal(modal, pugnaOverlay);
+    });
+  });
+}
+
+function dismissModal(modal, overlay) {
+  overlay.classList.remove("show");
+  modal.classList.remove("show");
+}
